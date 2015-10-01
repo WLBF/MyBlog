5dc8595349974da8d92cbd8b670039fb2b091c3d
WLBF <lbf1353@live.com>
2015-10-01 23:27:19 +0800

    5

--- MyBlog/urls.py
+++ MyBlog/urls.py
@@ -15,6 +15,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'MyBlog.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', include('wlbf.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^wlbf/', include('wlbf.urls')),
     url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
